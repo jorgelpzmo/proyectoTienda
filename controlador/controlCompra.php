ab2e5aca1f47ea43706149d9063952c0abb48640
NicoBuquetCebada <nicobuquetcebada@gmail.com>
2024-11-09 22:00:40 +0100

    subido control compra

--- controlador/controlCompra.php
+++ controlador/controlCompra.php
@@ -1,3 +1,37 @@
 <?php 
+	require '../modelo/DAOCompra.php';
+	class ControlCompra {
+
+		private $DAOCompra;
+
+		public function __construct() {
+			$this->DAOCompra = new DAOCompra();
+		}
+
+		public function getCompra($id) {
+			return $this->DAOCompra->selectCompra($id);
+		}
+
+		public function getAllCompras() {
+			return $this->DAOCompra->selectAll();
+		}
+
+		public function nuevoCompra($Compra) {
+			return $this->DAOCompra->insertCompra($Compra);
+		}
 	
+		public function borrarCompra($id) {
+			if(self::getCompra($id)) {
+				$this->DAOCompra->deleteCompra($id);
+				return 1;
+			}
+			else {
+				return -1;
+			}
+		}
+	
+		public function actualizarCompra($Compra) {
+			return $this->DAOCompra->updateCompra($Compra);
+		}
+	}
 ?>
--- modelo/DAOCompra.php
+++ modelo/DAOCompra.php
@@ -6,14 +6,14 @@
 			$this->con = Singleton::getCon();
 		}
 
-		function insertCompra($usuario_id, $producto_id, $fecha_compra, $id, $cantidad) {
+		function insertCompra($compra) {
 			$ps = $this->con->prepare("INSERT INTO compras (usuario_id, producto_id, fecha_compra, id, cantidad) VALUES (:usuario_id, :producto_id, :fecha_compra, :id, :cantidad)");
 
-			$ps->bindParam(":usuario_id", $usuario_id);
-			$ps->bindParam(":producto_id", $producto_id);
-			$ps->bindParam(":fecha_compra", $fecha_compra);
-			$ps->bindParam(":id", $id);
-			$ps->bindParam(":cantidad", $cantidad);
+			$ps->bindParam(":usuario_id", $compra->getUsuarioId());
+			$ps->bindParam(":producto_id", $compra->getProductoId());
+			$ps->bindParam(":fecha_compra", $compra->getFechaCompra());
+			$ps->bindParam(":id", $compra->getId());
+			$ps->bindParam(":cantidad", $compra->getCantidad());
 			$success = $ps->execute();
 			
 			if ($success)
