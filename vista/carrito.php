b8914eb6d28db27b4dde78054083b7747d817b7d
NicoBuquetCebada <nicobuquetcebada@gmail.com>
2024-11-09 21:45:08 +0100

    carrito

---
+++ vista/carrito.php
@@ -0,0 +1,46 @@
+<?php 
+	require '../modelo/DTOProducto.php';
+	
+	session_name('carrito');
+	session_start();
+	//session_unset();
+	$total = 0;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>montana</title>
+</head>
+<body>
+	<header>
+		<section id="logo"><a href="index.php">logo</a></section>
+		<section id="inicio"><a href="index.php">inicio</a></section>
+		<section id="carrito"><a href="carrito.php">carrito</a></section>
+		<section id="user"><a href="usuario.php">usuario</a></section>
+		<section id="trastienda"><a href="menuTrastienda.php">trastienda</a></section>
+	</header>
+	<div id="carrito">
+		<?php for ($i = 0; $i < count($_SESSION['productos']); $i++):?>
+			<?php $total += $_SESSION['productos'][$i]->getPrecio() * $_SESSION['cantidad'][$i] ?>
+			<article>
+				<img src="" alt="">
+				<div>
+					<p><?= $_SESSION['productos'][$i]->getNombre() . " " . $_SESSION['cantidad'][$i] . " " . $_SESSION['productos'][$i]->getPrecio()?></p>
+				</div>
+			</article>
+		<?php endfor; ?>
+		<h2><?= "TOTAL: " . $total ?></h2>
+		<input type="">
+	</div>
+	<footer>
+		<section id="social">
+
+		</section>
+		<section id="foot_info">
+
+		</section>
+	</footer>
+</body>
+</html>
