c5cb930703b899029908ed8db6825957133f4a0c
NicoBuquetCebada <nicobuquetcebada@gmail.com>
2024-11-16 00:04:08 +0100

    awraf

--- controlador/controladorPedido.php
+++ controlador/controladorPedido.php
@@ -8,34 +8,24 @@ session_start();
 
 $mensaje="";
 
-if(!isset($_REQUEST["id"])|| empty($_REQUEST["id"])){
-    $mensaje = "Campo id vacio";
-    header("Location: ../vista/vistaPedido.php?mensaje=$mensaje");
-    exit;
-}
-
-if(!isset($_REQUEST["cantidad"]) || empty($_REQUEST["cantidad"])){
-    $mensaje = "Campo cantidad vacio";
-    header("Location: ../vista/vistaPedido.php?mensaje=$mensaje");
-    exit;
-}
-
-
 $id = isset($_REQUEST["id"]) ? $_REQUEST["id"] : null;
 $cantidad = isset($_REQUEST["cantidad"]) ? $_REQUEST["cantidad"] : null;
-
-
-
 
 $control_producto=new ControlProducto();
 $producto = $control_producto->getProducto($id);
 
-
-
-//HE CREADO PRIMERO UN BUCLE QUE RECORRE LA SESION PRODUCTO, PARA VER SI YA SE HABIA AÑADIDO ESE PRODUCTO, PARA EN VEZ DE CREAR 2 ENTRADAS DEL MISMO QUE SE ACTUALICE EL STOCK. DESPUES, NO NECESITAS SESION CANTIDAD, YA EN EL PRODUCTO, CUANDO LO INTRODUCES, LITERALMENTE ESTABAS ACTUALIZANDO EL STOCK. LO UNICO QUE TENDRIAS QUE HACER ES UNA VEZ HECHO ESTO, ACTUALIZAR EL STOCK POR EL NUEVO EN LA BASE DE DATOS, LO QUE HACIAS EN AÑADIR, ERA VOLVER A SUMARSELO 2 VECES O ALGO ASI, PAR HACER LO QUE TE DIGO HE HECHO EL METODO REPONER EN CONTROL PRODUCTO, QUE LO QUE HACE BASICAMENTE ES SETEAR EL STOCK AL STOCK QUE TENGA EL OBJETO QUE LE PASAS POR PARAMETRO. BASICAMENTE LO HACIAS BIEN PERO HACIAS COMO 10 COSAS DE MAS.
-
 if ($_REQUEST["action"] == "+") { //ESTE IF ESTABA 2 VECES
-    //LO HE PUESTO PRIMERO PORQUE ES EL PRIMER BOTÓN, ME AYUDA A VERLO MEJOR
+    if(!isset($_REQUEST["id"])|| empty($_REQUEST["id"])){
+        $mensaje = "Campo id vacio";
+        header("Location: ../vista/vistaPedido.php?mensaje=$mensaje");
+        exit;
+    }
+
+    if(!isset($_REQUEST["cantidad"]) || empty($_REQUEST["cantidad"])){
+        $mensaje = "Campo cantidad vacio";
+        header("Location: ../vista/vistaPedido.php?mensaje=$mensaje");
+        exit;
+    }
     if (!$producto) {
         $mensaje = "Añada producto antes de pedir";
         header("Location: ../vista/vistaPedido.php?mensaje=$mensaje");
